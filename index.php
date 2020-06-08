1a184e381fb0b8842f3efe0acc31ebcf5967c01a
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 16:00:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    function concat($str, $nb){
+        echo $str . ' a ' . $nb . ' ans';
+        } 
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 4 exercice 5</title>
+    </head>
+    <body>
+        <p><?php concat('tony', 24) ?></p>
+    </body>
+</html>
